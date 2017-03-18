13bd70e850393bd1f45ac1a0c64cfed4405534f8
kwy8791 <kwy8791+gh@gmail.com>
2017-03-18 23:11:13 +0900

    a little sytax error is fixed

--- bin/audit/create_centos_user_list.sh
+++ bin/audit/create_centos_user_list.sh
@@ -7,7 +7,7 @@ echo "user,last login, id" > /tmp/`date +'%F'`-OS-`hostname`.lst
 for OS_USER in `grep -v '/sbin/nologin' /etc/passwd |awk -F':' '{print $1}'` ;do
 	echo -n "`lastlog -u ${OS_USER} |grep -v '^Username'`   "
 	id ${OS_USER}
-done |sed -e "s/pts\/[0-9]\?//g" |sed -e "s/  \+/,/g" >> /tmp/`date +'%F'`-OS-hostname.lst
+done |sed -e "s/pts\/[0-9]\?//g" |sed -e "s/  \+/,/g" >> /tmp/`date +'%F'`-OS-`hostname`.lst
 
 echo "List of OS user and privilege file: /tmp/`date +'%F'`-OS-hostname.lst"
 
